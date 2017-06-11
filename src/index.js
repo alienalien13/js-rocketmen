757dcf334b918abe34894d4d938f5cd361a61c70
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-06-11 18:18:39 +0200

    all forms validation

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ var objForms = {
 		}
 	}
 }
-
+var validateLatin = function(option){
+		return regLatin.test(option);
+	}
 var values = {
 	name: '',
 	lastname: '',
@@ -25,36 +27,32 @@ var values = {
 	superabil: ''
 }
 
+
+
 var lol = () => { //потом эта функция принимает объект в котором будет возиться, сначала это данные форм, потом это буду данные уже заполненной таблицы (для эдитации)
-	for (let option in objForms.getValues()){
-		
-		if (objForms.getValues()[option] === ''){
-			let form = document.getElementById(option);
-			form.style.background = 'red';
-			console.log('please, FILL IN: ' + option)
-		} else {
+	if (validateLatin(objForms.getValues().name) === true && validateLatin(objForms.getValues().lastname) === true && objForms.getValues()[birth] !== '' && objForms.getValues()[superabil] !== ''){
+		values.name = objForms.getValues().name;
+		values.lastname = objForms.getValues().lastname;
+		values.birth = objForms.getValues().birth;
+		values.superabil = objForms.getValues().superabil;
+		for (let option in objForms.getValues()){
 			let form = document.getElementById(option);
 			form.style.background = 'none';
-			var result = regLatin.test(objForms.getValues()[option]);
-
-			/*if (option === 'name' && result === true){
-				values.name = objForms.getValues()[option]
-			} else if (option === 'lastname' && result === true) {
-				values.lastname = objForms.getValues()[option]
-			}*/
-
-			/*if (option !== 'birth' && option !== 'superabil' && result === true) {
-				console.log(option + ' ' + objForms.getValues()[option] + ' good ')
-			} else if (option !== 'birth' && option !== 'superabil' && result === false) {
-				console.log(option + ' ' + objForms.getValues()[option] + ' Latin letters ONLY ')
-			}*/
+		}
+		console.log(values)
+	} else {
+		for (let option in objForms.getValues()){
+			let form = document.getElementById(option);
+			form.style.background = 'none';
+			if (validateLatin(objForms.getValues()[option]) === false){
+				form.style.background = 'red';
+				if (option === 'birth' && objForms.getValues()[option] !== ''){
+					form.style.background = 'none';
+				}
+				console.log(option);
+			}
 		}
 	}
 }
 
 buttonAdd.onclick = lol;
-//$('.buttonAdd').on('click', console.log(lol()))
-
-
-
-
